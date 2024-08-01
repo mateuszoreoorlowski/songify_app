57f20ce1931203feba4d96e2e4fd797e1a61f7a8
Mateusz <mateusz201134@gmail.com>
2024-08-01 09:57:40 +0200

    Initial commit

---
+++ src/main/java/spring/songify_app/SongifyAppApplication.java
@@ -0,0 +1,13 @@
+package spring.songify_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongifyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongifyAppApplication.class, args);
+    }
+
+}
