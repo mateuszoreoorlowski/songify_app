89fcda107e160319f1cf367d0718f3d92f35e700
Mateusz <mateusz201134@gmail.com>
2024-08-01 17:31:27 +0200

    update endpoints 'add song', now song has default genre

---
+++ src/main/java/spring/songify_app/domain/crud/GenreAssigner.java
@@ -0,0 +1,18 @@
+package spring.songify_app.domain.crud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+class GenreAssigner {
+
+    private final SongRetriever songRetriever;
+    private final GenreRetriever genreRetriever;
+
+    void assignDefaultGenreToSong(Long songId) {
+        Song song = songRetriever.findSongById(songId);
+        Genre genre = genreRetriever.findById(1L);
+        song.setGenre(genre);
+    }
+}
--- src/main/java/spring/songify_app/domain/crud/SongAdder.java
+++ src/main/java/spring/songify_app/domain/crud/SongAdder.java
@@ -11,12 +11,22 @@ import spring.songify_app.infrastructure.crud.song.request.CreateSongRequestDto;
 class SongAdder {
 
     private final SongRepository songRepository;
+    private final GenreRepository genreRepository;
+    private final GenreAssigner genreAssigner;
 
     SongDto addSong(final CreateSongRequestDto songDto) {
         SongLanguageDto language = songDto.language();
         SongLanguage songLanguage = SongLanguage.valueOf(language.name());
         Song song = new Song(songDto.name(), songDto.releaseDate(), songDto.duration(), songLanguage);
         Song save = songRepository.save(song);
+
+        // Ensure default genre exists before assigning
+        if (!genreRepository.existsById(1L)) {
+            Genre defaultGenre = new Genre("default");
+            genreRepository.save(defaultGenre);
+        }
+
+        genreAssigner.assignDefaultGenreToSong(save.getId());
 
         return new SongDto(save.getId(), save.getName(), save.getDuration(), save.getReleaseDate(), language);
     }
