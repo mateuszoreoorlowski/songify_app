2acebc2b44baac63131df254a14cf535ebbbb155
Mateusz <mateusz201134@gmail.com>
2024-08-05 18:42:56 +0200

    added integration test directory and refactoring migrations

---
+++ src/integration/java/feature/HappyPathIntegrationTest.java
@@ -0,0 +1,31 @@
+package feature;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import spring.songify_app.SongifyAppApplication;
+
+@SpringBootTest(classes = SongifyAppApplication.class)
+@Testcontainers
+@AutoConfigureMockMvc
+@ActiveProfiles("integration")
+class HappyPathIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @DynamicPropertySource
+    public static void propertyOverride(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+    }
+}
